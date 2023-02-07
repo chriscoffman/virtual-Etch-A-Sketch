78ca852de067cc4decffdef2feb35e46bdc8a642
chriscoffman <chris.coffman2@gmail.com>
2023-02-07 11:58:54 -0600

    add button to reset grid and change pizel size

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-let length  = 100;
 function createGrid(sidelength) {
     for (let i = 0; i <sidelength; i++) {
         const row = document.createElement('div');
@@ -12,6 +10,9 @@ function createGrid(sidelength) {
             row.appendChild(box);
         } 
     }
+    const mySquareList = document.querySelectorAll('.square');
+    mySquareList.forEach(square => square.addEventListener('mouseover',
+    () => hoverChangeColor(square)));
 }
 
 function hoverChangeColor(element) {
@@ -19,10 +20,24 @@ function hoverChangeColor(element) {
     console.log(element);
 }
 
+function selectGridSize() {
+    let size = prompt("Please select grid size, no larger than 100", 16);
+    size = parseInt(size);
+    if (isNaN(size)) {
+        selectGridSize();
+    }
+    clearGrid();
+    createGrid(size);
+}
 
+function clearGrid() {
+    container.innerHTML = '';
+}
+
+let length  = 16;
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+button.addEventListener('click', ()=> selectGridSize());
 createGrid(length);
-const mySquareList = document.querySelectorAll('.square');
-mySquareList.forEach(square => square.addEventListener('mouseover',
-() => hoverChangeColor(square)));
+
 
