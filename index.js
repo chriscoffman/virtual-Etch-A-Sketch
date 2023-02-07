b208b3883966d5c7e60757c62eb584ad0aae1994
chriscoffman <chris.coffman2@gmail.com>
2023-02-07 11:09:04 -0600

    add color change on hover functionality to grid

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 
-let length  = 16;
+let length  = 100;
 function createGrid(sidelength) {
     for (let i = 0; i <sidelength; i++) {
         const row = document.createElement('div');
+        row.classList.add("row");
         container.appendChild(row);
         
         for(let i = 0; i<sidelength; i++) {
             const box = document.createElement('div');
-            box.textContent = 'hi';
+            box.classList.add("square");
             row.appendChild(box);
         } 
     }
 }
 
+function hoverChangeColor(element) {
+    element.classList.add("hover-color")
+    console.log(element);
+}
 
-const container = document.querySelector('#container');
+
+const container = document.querySelector('.container');
 createGrid(length);
+const mySquareList = document.querySelectorAll('.square');
+mySquareList.forEach(square => square.addEventListener('mouseover',
+() => hoverChangeColor(square)));
 
